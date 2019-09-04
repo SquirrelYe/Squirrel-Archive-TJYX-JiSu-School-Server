177ba1807d6bf895d0b4b3fe0944995ce6159387
willye <willye@tencent.com>
2019-09-04 20:45:03 +0800

    2019.09.04 增加本地测试环境 腾讯公司电脑

--- entity/user.js
+++ entity/user.js
@@ -43,141 +43,137 @@ module.exports = {
     // 模型实体
     user,
     // 查询所有
-    findAndCountAll(req,res){
-        user.findAndCountAll().then( msg => { res.send(msg) })       
+    findAndCountAll(req, res) {
+        user.findAndCountAll().then(msg => { res.send(msg) })
     },
     //查询注册时邮箱是否被占用
-    selectUsersByEmail(req,res){
-        user.findAndCountAll({ 
-            'where':{ 'mail':req.body.mail }
-         }).then( msg=>{ res.send(msg)  })       
-    },  
+    selectUsersByEmail(req, res) {
+        user.findAndCountAll({
+            'where': { 'mail': req.body.mail }
+        }).then(msg => { res.send(msg) })
+    },
     //登录
-    login(req,res){
+    login(req, res) {
         user.findOne({
-            'where':{
-                'name':req.body.name,
-                'pass':req.body.pass
-            },
-            include: [{ model: info }, { model: authen }, { model: school }, { model: stock }],
-        })
-        .then( msg=>{ 
-            if(msg){
-                // 利用openid 生成token 并返回 token
-                const { id,openid } = msg.dataValues
-                let encrp = jwt.sign( {openid} , secret ,{ expiresIn:'1h'})
-                let data = { ...msg.dataValues, token:encrp}                
-                // redis保存登录态
-                console.log('redis',id,encrp)
-                db.set(`tjyxlogin-${id}`,encrp,null,(err,result)=>{
-                    if(err){
-                        res.status(251).send("redis服务器异常！！！");
-                    }else{
-                        res.send(data)
-                    }
-                })
-            }
-            else res.status(432).send("登录校验失败") 
-         })
-    },    
+                'where': {
+                    'name': req.body.name,
+                    'pass': req.body.pass
+                },
+                include: [{ model: info }, { model: authen }, { model: school }, { model: stock }],
+            })
+            .then(msg => {
+                if (msg) {
+                    // 利用openid 生成token 并返回 token
+                    const { id, openid } = msg.dataValues
+                    let encrp = jwt.sign({ openid }, secret, { expiresIn: '1h' })
+                    let data = {...msg.dataValues, token: encrp }
+                        // redis保存登录态
+                    console.log('redis', id, encrp)
+                    db.set(`tjyxlogin-${id}`, encrp, null, (err, result) => {
+                        if (err) {
+                            res.status(251).send("redis服务器异常！！！");
+                        } else {
+                            res.send(data)
+                        }
+                    })
+                } else res.status(432).send("登录校验失败")
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    },
     //注册管理员
-    create(req,res){
+    create(req, res) {
         user.findOrCreate({
             where: {
-                'name':req.body.name,
-                'mail':req.body.mail
+                'name': req.body.name,
+                'mail': req.body.mail
             },
-            defaults: { 
-                'openid':req.body.openid,
-                'sessionid':req.body.sessionid,
-                'name':req.body.name,
-                'pass':req.body.pass,
-                'type':req.body.type,
-                'mail':req.body.mail,
-                'phone':req.body.phone,
-                'condition':req.body.condition,
-                'school_id':req.body.school_id,
+            defaults: {
+                'openid': req.body.openid,
+                'sessionid': req.body.sessionid,
+                'name': req.body.name,
+                'pass': req.body.pass,
+                'type': req.body.type,
+                'mail': req.body.mail,
+                'phone': req.body.phone,
+                'condition': req.body.condition,
+                'school_id': req.body.school_id,
             }
-        }).then( msg=>{ res.send(msg); })
+        }).then(msg => { res.send(msg); })
     },
     //更新密码（密码找回）
-    updatePass(req,res){
-        user.update(
-            { 'pass':req.body.pass },
-            {
-                'where':{ 'mail':req.body.mail }
-            }).then( msg=>{ res.send(msg); })
+    updatePass(req, res) {
+        user.update({ 'pass': req.body.pass }, {
+            'where': { 'mail': req.body.mail }
+        }).then(msg => { res.send(msg); })
     },
     //删除用户
-    delete(req,res){
-        user.destroy(
-            {
-                where:{ 'id':req.body.id }
-            }
-        ).then( msg=>{ res.send({'affectRows':msg}); })
+    delete(req, res) {
+        user.destroy({
+            where: { 'id': req.body.id }
+        }).then(msg => { res.send({ 'affectRows': msg }); })
     },
     //更新用户信息
-    update(req,res){
-        user.update(
-            {  
-                'openid':req.body.openid,
-                'sessionid':req.body.sessionid,
-                'name':req.body.name,
-                'pass':req.body.pass,
-                'type':req.body.type,
-                'mail':req.body.mail,
-                'phone':req.body.phone,
-                'condition':req.body.condition,
-                'school_id':req.body.school_id,
-            },
-            {   'where':{ 'id':req.body.id }
-        }).then( msg=>{ res.send(msg); })
+    update(req, res) {
+        user.update({
+            'openid': req.body.openid,
+            'sessionid': req.body.sessionid,
+            'name': req.body.name,
+            'pass': req.body.pass,
+            'type': req.body.type,
+            'mail': req.body.mail,
+            'phone': req.body.phone,
+            'condition': req.body.condition,
+            'school_id': req.body.school_id,
+        }, {
+            'where': { 'id': req.body.id }
+        }).then(msg => { res.send(msg); })
     },
     // 注册用户
-    cusCreate(req,res){
+    cusCreate(req, res) {
         user.findOrCreate({
             where: {
-                'phone':req.body.phone
+                'phone': req.body.phone
             },
-            defaults: { 
-                'openid':req.body.openid,
-                'sessionid':req.body.sessionid,
-                'name':req.body.name,
-                'pass':req.body.pass,
-                'type':0,
-                'mail':req.body.mail,
-                'phone':req.body.phone,
-                'condition':0,
-                'school_id':req.body.school_id,
+            defaults: {
+                'openid': req.body.openid,
+                'sessionid': req.body.sessionid,
+                'name': req.body.name,
+                'pass': req.body.pass,
+                'type': 0,
+                'mail': req.body.mail,
+                'phone': req.body.phone,
+                'condition': 0,
+                'school_id': req.body.school_id,
             }
-        }).then( msg=>{ res.send(msg); })
+        }).then(msg => { res.send(msg); })
     },
     // 用户登录
-    cusLogin(req,res){
+    cusLogin(req, res) {
         user.findOne({
-            'where':{
-                'phone':req.body.phone,
-                'pass':req.body.pass
-            },
-            include: [{ model: info }, { model: authen }, { model: school }, { model: stock }],
-        })
-        .then( msg=>{ 
-            if(msg){
-                // 利用openid 生成token 并返回 token
-                const { id,openid } = msg.dataValues
-                let encrp = jwt.sign( {openid} , secret ,{ expiresIn:'1h'})
-                let data = { ...msg.dataValues, token:encrp}                
-                // redis保存登录态
-                console.log('redis',id,encrp)
-                db.set(`tjyxlogin-${id}`,encrp,null,(err,result)=>{
-                    if(err){
-                        res.status(251).send("redis服务器异常！！！");
-                    }else{
-                        res.send(data)
-                    }
-                })
-            }
-            else res.status(432).send("登录校验失败") 
-         })
-    }, 
+                'where': {
+                    'phone': req.body.phone,
+                    'pass': req.body.pass
+                },
+                include: [{ model: info }, { model: authen }, { model: school }, { model: stock }],
+            })
+            .then(msg => {
+                if (msg) {
+                    // 利用openid 生成token 并返回 token
+                    const { id, openid } = msg.dataValues
+                    let encrp = jwt.sign({ openid }, secret, { expiresIn: '1h' })
+                    let data = {...msg.dataValues, token: encrp }
+                        // redis保存登录态
+                    console.log('redis', id, encrp)
+                    db.set(`tjyxlogin-${id}`, encrp, null, (err, result) => {
+                        if (err) {
+                            res.status(251).send("redis服务器异常！！！");
+                        } else {
+                            res.send(data)
+                        }
+                    })
+                } else res.status(432).send("登录校验失败")
+            })
+    },
 };
--- orm/orm.js
+++ orm/orm.js
@@ -1,27 +1,32 @@
 const mysql = require('mysql');
-const Sequelize=require('sequelize')
-const log=require('../log/log')
+const Sequelize = require('sequelize')
+const log = require('../log/log')
 
 module.exports = {
     //sequelize ORM对象关系映射 
-    connection:function(req,res){
+    connection: function(req, res) {
         var sequelize = new Sequelize(
             'TJYX_JiSu_School', // 数据库名
             'user',   // 用户名
             'yexuan@0628',   // 用户密码
-            // 'yexuan@0628',  //服务器数据库
+
+            // 'tjyx_jsxy_test',
+            // 'root',
+            // 'yexuan0628',
+
             {
-                'dialect': 'mysql',  // 数据库使用mysql
+                'dialect': 'mysql', // 数据库使用mysql
                 'host': '42.81.142.18', // 数据库服务器ip
-                'port': 3306,        // 数据库服务器端口
+                // 'host': '127.0.0.1', // 数据库服务器ip
+                'port': 3306, // 数据库服务器端口
                 'define': {
                     // 字段以下划线（_）来分割（默认是驼峰命名风格）
                     'underscored': true
                 },
                 timezone: '+08:00',
-                logging:log.info
+                logging: log.info
             }
         )
         return sequelize;
     }
-} 
+}
